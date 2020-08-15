cb73751f0e3fa54a201ac513664e8dda08fdeb53
mengyue <m15032620398@163.com>
2020-08-15 17:07:12 +0800

    2222

--- src/main/java/io/github/talelin/latticy/controller/v1/HelloWord.java
+++ src/main/java/io/github/talelin/latticy/controller/v1/HelloWord.java
@@ -5,4 +5,7 @@ package io.github.talelin.latticy.controller.v1;
  * @create 2020-08-15 2020/8/15
  */
 public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("1111");
+    }
 }

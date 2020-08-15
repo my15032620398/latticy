166d1eb227a703d26ce9846bf789fab414ac1bc7
mengyue <m15032620398@163.com>
2020-08-15 17:08:57 +0800

    2020-08-15

--- src/main/java/io/github/talelin/latticy/controller/v1/HelloWord.java
+++ src/main/java/io/github/talelin/latticy/controller/v1/HelloWord.java
@@ -7,5 +7,6 @@ package io.github.talelin.latticy.controller.v1;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("1111");
+        System.out.println("2222");
     }
 }
